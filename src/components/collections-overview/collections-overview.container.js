75c01226296ec890c8886f7d61afdc7c586d4da3
Vigan Zeqiri <viganzeqirii@gmail.com>
2020-02-29 09:15:07 +0100

    Apollo client for fetching data and managing state

---
+++ src/components/collections-overview/collections-overview.container.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
+
+import CollectionOverview from "../collections-overview/collections-overview.component";
+import Spinner from "../spinner/spinner.component";
+
+const GET_COLLECTIONS = gql`
+	{
+		collections {
+			id
+			title
+			items {
+				id
+				name
+				price
+				imageUrl
+			}
+		}
+	}
+`;
+
+const CollectionsOverviewContainer = () => {
+	return (
+		<Query query={GET_COLLECTIONS}>
+			{({ loading, error, data }) => {
+				if (loading) return <Spinner />;
+				return <CollectionOverview collections={data.collections} />;
+			}}
+		</Query>
+	);
+};
+
+export default CollectionsOverviewContainer;
---
+++ src/components/header/header.container.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
+
+import Header from "./header.component";
+
+const GET_CART_HIDDEN = gql`
+	{
+		cartHidden @client
+	}
+`;
+
+const HeaderContainer = () => {
+	return (
+		<Query query={GET_CART_HIDDEN}>
+			{({ data }) => {
+				const { cartHidden } = data;
+				return <Header hidden={cartHidden} />;
+			}}
+		</Query>
+	);
+};
+
+export default HeaderContainer;
